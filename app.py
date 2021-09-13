677ca7bd19428ee87b64367578bfb5004542841e
Preeti Sharma <70448973+PreetiSharma15@users.noreply.github.com>
2021-09-13 15:27:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import date
 
 import streamlit as st
 
-import tensorflow as tf
+import tensorflow
 
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
